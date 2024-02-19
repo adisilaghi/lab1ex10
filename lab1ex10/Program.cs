bb11c76d97f9b6d2eed0f0a9d3d5c6d3a94f1141
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 22:33:36 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la
+tastatura. Numarul are exact 2 cifre*/
+
+
+
+Console.WriteLine("Introduceti un numar din 2 cifre");
+int nr = Convert.ToInt32(Console.ReadLine());
+int primacifra=nr/10;
+int doileacifra=nr%10;
+int suma=primacifra+doileacifra;
+
+Console.WriteLine("Suma cifrelor este: "+suma);
